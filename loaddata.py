53c938d89fd8827e4503adb4ba51d00bd843e7a1
MaschinenNah <81768073+MaschinenNah@users.noreply.github.com>
2021-04-21 07:39:18 +0200

    Update loaddata.py

--- loaddata.py
+++ loaddata.py
@@ -3,6 +3,6 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 def load_and_unzip(zipurl):
-with urlopen(zipurl) as zipresp:
-  with ZipFile(BytesIO(zipresp.read())) as zfile:
-    zfile.extractall()
+  with urlopen(zipurl) as zipresp:
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+      zfile.extractall()
