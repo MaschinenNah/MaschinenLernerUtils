ea897cd312c08ff80dc5620c8fafb570a27a11d8
MaschinenNah <81768073+MaschinenNah@users.noreply.github.com>
2021-04-21 07:37:49 +0200

    Update loaddata.py

--- loaddata.py
+++ loaddata.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from urllib.request import urlopen
 from zipfile import ZipFile
 
-def load_and_unzip(zipurl)
+def load_and_unzip(zipurl):
 with urlopen(zipurl) as zipresp:
   with ZipFile(BytesIO(zipresp.read())) as zfile:
     zfile.extractall()
