26acfc7fc83afbf56f2accb2a89e2f2652ed8095
MaschinenNah <81768073+MaschinenNah@users.noreply.github.com>
2021-04-21 11:51:02 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ from PIL import Image
 import numpy as np
 
 def load_and_unzip(zipurl):
-"""Lädt das zipfile unter zipurl und entpackt es ins aktuelle Arbeitsverzeichnis"""
+"""Lädt das zipfile unter zipurl und entpackt es ins aktuelle Arbeitsverzeichnis
+"""
   with urlopen(zipurl) as zipresp:
     with ZipFile(BytesIO(zipresp.read())) as zfile:
       zfile.extractall()
 
 def img_path_to_float_array(path):
-"""Lädt eine unter path gespeicherte Bilddatei und erzeugt ein entsprechendes Numpy Array vom Typ float32 mit Wertebereich 0.0 - 1.0"""
+"""Lädt eine unter path gespeicherte Bilddatei und erzeugt ein entsprechendes Numpy Array vom Typ float32 mit Wertebereich 0.0 - 1.0
+"""
   img = Image.open(path)
   img_as_array = np.array(img)
   return (img_as_array / 255.0).astype("float32")
